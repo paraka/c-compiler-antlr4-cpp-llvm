68cfc5b34fc55ec12004be18611de884e216bf6c
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2017-12-17 16:14:57 +0100

    src: add simple but real main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <antlr4-runtime.h>
-#include "CParser.h"
+#include <CLexer.h>
+#include <CParser.h>
+
+using namespace antlr4;
+using namespace antlrcpptest;
 
 int main(void) 
 {
-    std::cout << "Hello World" << std::endl;
+    std::string line;
+    std::ifstream sample("../samples/simple.c");
+
+    if (sample.is_open()) 
+    {
+        ANTLRInputStream input(sample);
+        CLexer lexer(&input);
+        CommonTokenStream tokens(&lexer);
+
+        tokens.fill();
+
+        for (const auto &token : tokens.getTokens()) 
+            std::cout << "TOKEN: " << token->toString() << std::endl;
+
+        CParser parser(&tokens);
+        tree::ParseTree *tree = parser.compilationUnit();
+        std::cout << tree->toStringTree(&parser) << std::endl;
+        sample.close();
+    }
 
     return 0;
 }
