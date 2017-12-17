db1cd84717145d6d9a14bb73badaa38519c90761
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2017-12-17 16:42:23 +0100

    samples: add samples directory for input program samples

---
+++ samples/simple.c
@@ -0,0 +1,21 @@
+/* simple program */
+
+extern int f;
+
+void function(int x, int y)
+{
+    int f;
+
+    while (x < y)
+    {
+        f++;
+        x++;
+    }
+}
+
+int main(void)
+{
+    printf("Hello world\n");
+
+    return 0;
+}
